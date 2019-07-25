c2b901d44b11e6244202a28235e8bfabb30b4126
Leeheejin1 <dlgmlwls3133@naver.com>
2019-07-25 15:54:02 +0900

    last

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blogproject.wsgi.application'
+# WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 
 # Database
@@ -119,7 +119,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
